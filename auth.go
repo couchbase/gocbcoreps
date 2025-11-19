fa952f7a9e3fcf61301fcfdf730cb7df2ee8000f
Charles Dixon <chvckd@gmail.com>
2025-11-19 15:53:55 +0000

    GOCBC-1769: Change to Authenticator interface
    Motivation
    ----------
    We need a way to do mtls rotation and also to support JWT auth.
    To achieve this we need to refactor how auth is done.
    This is technically a breaking change but we are not aware of any
    users other than gocb.
    
    Changes
    -------
    Remove username, password, and certificate.
    Create Authenticator interface and certificate and basic authenticators.
    Expose a ReconfigureAuthenticator function on routing client.

--- auth.go
+++ auth.go
@@ -2,28 +2,69 @@ package gocbcoreps
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/base64"
-
-	"google.golang.org/grpc/credentials"
+	"sync/atomic"
 )
 
-type GrpcBasicAuth struct {
-	EncodedData string
+type Authenticator interface {
+	isAuthenticator()
 }
 
-// NewJWTAccessFromKey creates PerRPCCredentials from the given jsonKey.
-func NewGrpcBasicAuth(username, password string) (credentials.PerRPCCredentials, error) {
+type BasicAuthenticator struct {
+	encodedData atomic.Pointer[string]
+}
+
+// NewBasicAuthenticator creates PerRPCCredentials from the given username and password.
+func NewBasicAuthenticator(username, password string) *BasicAuthenticator {
 	basicAuth := username + ":" + password
 	authValue := base64.StdEncoding.EncodeToString([]byte(basicAuth))
-	return GrpcBasicAuth{authValue}, nil
+
+	auth := &BasicAuthenticator{}
+
+	auth.encodedData.Store(&authValue)
+
+	return auth
 }
 
-func (j GrpcBasicAuth) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+func (j *BasicAuthenticator) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	encodedData := j.encodedData.Load()
 	return map[string]string{
-		"authorization": "Basic " + j.EncodedData,
+		"authorization": "Basic " + *encodedData,
 	}, nil
 }
 
-func (j GrpcBasicAuth) RequireTransportSecurity() bool {
+func (j *BasicAuthenticator) RequireTransportSecurity() bool {
 	return false
 }
+
+func (j *BasicAuthenticator) UpdateCredentials(username, password string) {
+	basicAuth := username + ":" + password
+	authValue := base64.StdEncoding.EncodeToString([]byte(basicAuth))
+
+	j.encodedData.Store(&authValue)
+}
+
+func (j *BasicAuthenticator) isAuthenticator() {}
+
+type CertificateAuthenticator struct {
+	certificate atomic.Pointer[tls.Certificate]
+}
+
+func NewCertificateAuthenticator(cert *tls.Certificate) *CertificateAuthenticator {
+	auth := &CertificateAuthenticator{}
+	auth.certificate.Store(cert)
+	return auth
+}
+
+func (j *CertificateAuthenticator) GetClientCertificate(info *tls.CertificateRequestInfo) (*tls.Certificate, error) {
+	cert := j.certificate.Load()
+
+	return cert, nil
+}
+
+func (j *CertificateAuthenticator) UpdateCertificate(cert *tls.Certificate) {
+	j.certificate.Store(cert)
+}
+
+func (j *CertificateAuthenticator) isAuthenticator() {}
