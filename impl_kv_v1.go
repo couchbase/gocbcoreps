3cdd6a3a53cabf04ba638e0a9e2d48e0eadb2cc0
Jack Westwood <jack.westwood@couchbase.com>
2025-12-09 17:34:03 +0000

    ING-1353: Implement optimized grpc load balancer

--- impl_kv_v1.go
+++ impl_kv_v1.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/couchbase/goprotostellar/genproto/kv_v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type routingImpl_KvV1 struct {
@@ -15,69 +16,91 @@ type routingImpl_KvV1 struct {
 var _ kv_v1.KvServiceClient = (*routingImpl_KvV1)(nil)
 
 func (c *routingImpl_KvV1) Get(ctx context.Context, in *kv_v1.GetRequest, opts ...grpc.CallOption) (*kv_v1.GetResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Get(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) GetAndTouch(ctx context.Context, in *kv_v1.GetAndTouchRequest, opts ...grpc.CallOption) (*kv_v1.GetAndTouchResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().GetAndTouch(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) GetAndLock(ctx context.Context, in *kv_v1.GetAndLockRequest, opts ...grpc.CallOption) (*kv_v1.GetAndLockResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().GetAndLock(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Unlock(ctx context.Context, in *kv_v1.UnlockRequest, opts ...grpc.CallOption) (*kv_v1.UnlockResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Unlock(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) GetAllReplicas(ctx context.Context, in *kv_v1.GetAllReplicasRequest, opts ...grpc.CallOption) (kv_v1.KvService_GetAllReplicasClient, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().GetAllReplicas(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Touch(ctx context.Context, in *kv_v1.TouchRequest, opts ...grpc.CallOption) (*kv_v1.TouchResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Touch(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Exists(ctx context.Context, in *kv_v1.ExistsRequest, opts ...grpc.CallOption) (*kv_v1.ExistsResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Exists(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Insert(ctx context.Context, in *kv_v1.InsertRequest, opts ...grpc.CallOption) (*kv_v1.InsertResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Insert(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Upsert(ctx context.Context, in *kv_v1.UpsertRequest, opts ...grpc.CallOption) (*kv_v1.UpsertResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Upsert(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Replace(ctx context.Context, in *kv_v1.ReplaceRequest, opts ...grpc.CallOption) (*kv_v1.ReplaceResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Replace(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Remove(ctx context.Context, in *kv_v1.RemoveRequest, opts ...grpc.CallOption) (*kv_v1.RemoveResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Remove(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Increment(ctx context.Context, in *kv_v1.IncrementRequest, opts ...grpc.CallOption) (*kv_v1.IncrementResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Increment(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Decrement(ctx context.Context, in *kv_v1.DecrementRequest, opts ...grpc.CallOption) (*kv_v1.DecrementResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Decrement(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Append(ctx context.Context, in *kv_v1.AppendRequest, opts ...grpc.CallOption) (*kv_v1.AppendResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Append(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) Prepend(ctx context.Context, in *kv_v1.PrependRequest, opts ...grpc.CallOption) (*kv_v1.PrependResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().Prepend(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) LookupIn(ctx context.Context, in *kv_v1.LookupInRequest, opts ...grpc.CallOption) (*kv_v1.LookupInResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().LookupIn(ctx, in, opts...)
 }
 
 func (c *routingImpl_KvV1) MutateIn(ctx context.Context, in *kv_v1.MutateInRequest, opts ...grpc.CallOption) (*kv_v1.MutateInResponse, error) {
+	ctx = c.contextWithRoutingInfo(ctx, in.BucketName, in.Key)
 	return c.client.fetchConnForKey(in.BucketName, in.Key).KvV1().MutateIn(ctx, in, opts...)
+}
+
+func (c *routingImpl_KvV1) contextWithRoutingInfo(ctx context.Context, bucketName, key string) context.Context {
+	md := metadata.Pairs("bucketname", bucketName, "key", key)
+	return metadata.NewOutgoingContext(ctx, md)
 }
