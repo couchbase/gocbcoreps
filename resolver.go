6b597821b27891e6ebe6c3a526b7962acd9d6ea4
Jack Westwood <jack.westwood@couchbase.com>
2025-12-09 17:34:03 +0000

    ING-1352: Bump to routingV2 and add custom name resolver

--- resolver.go
+++ resolver.go
@@ -15,11 +15,7 @@ import (
 	"google.golang.org/grpc/resolver"
 )
 
-<<<<<<< HEAD
 const OptimizedRoutingScheme = "couchbase2+optimized"
-=======
-const optimizedRoutingScheme = "optimized"
->>>>>>> 41db69a (ING-1352: Bump to routingV2 and add custom name resolver)
 
 const defaultResolveInterval = time.Second * 30
 
@@ -33,11 +29,7 @@ type CustomResolverBuilder struct {
 	resolveInterval time.Duration
 }
 
-<<<<<<< HEAD
 func (*CustomResolverBuilder) Scheme() string { return OptimizedRoutingScheme }
-=======
-func (*CustomResolverBuilder) Scheme() string { return optimizedRoutingScheme }
->>>>>>> 41db69a (ING-1352: Bump to routingV2 and add custom name resolver)
 
 func (c *CustomResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, rOpts resolver.BuildOptions) (resolver.Resolver, error) {
 	dialOpts := []grpc.DialOption{grpc.WithTransportCredentials(rOpts.DialCreds)}
